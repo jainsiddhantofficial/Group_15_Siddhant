9b25085f9d1807d21eca24c80ab1a57531c69017
Siddhant Jain <138900970+jainsiddhantofficial@users.noreply.github.com>
2025-12-04 12:15:26 +0530

    Add environment configuration file env.js

---
+++ Server/src/config/env.js
@@ -0,0 +1,21 @@
+// env.js
+require("dotenv").config();
+
+module.exports = {
+  PORT: process.env.PORT || 5000,
+
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASS: process.env.DB_PASS,
+  DB_NAME: process.env.DB_NAME,
+
+  JWT_SECRET: process.env.JWT_SECRET,
+
+  EMAIL_USER: process.env.EMAIL_USER,   // optional
+  EMAIL_PASS: process.env.EMAIL_PASS,   // optional
+
+  CLOUDINARY_KEY: process.env.CLOUDINARY_KEY || "",
+  CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET || "",
+
+  DEBUG: process.env.DEBUG === "true",
+};
